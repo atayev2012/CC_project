46ce76379b1a6e9539374250c3e16c2f0c643413
Batyr Atayev <atayev2012@gmail.com>
2024-04-24 06:03:40 +0300

    Added WEATHER_URL variable

--- config.py
+++ config.py
@@ -1,18 +1,19 @@
 from dotenv import load_dotenv
-import os
+from os import getenv
 
 load_dotenv()
 
 # Database variables
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+DB_USER = getenv("DB_USER")
+DB_PASSWORD = getenv("DB_PASSWORD")
+DB_NAME = getenv("DB_NAME")
+DB_HOST = getenv("DB_HOST")
+DB_PORT = getenv("DB_PORT")
 
 # Telegram variables
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN")
 
 
 # Tomorrow.io variables (weather service)
-WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+WEATHER_API_KEY = getenv("WEATHER_API_KEY")
+WEATHER_URL = getenv("WEATHER_URL")
