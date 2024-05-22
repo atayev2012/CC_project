5483b0b132e9b66fc5d16df7f844a0db90979ad8
Batyr Atayev <atayev2012@gmail.com>
2024-05-23 01:05:26 +0300

    Added command gender select

--- src/telegram_bot_app/handlers/user_command_handlers.py
+++ src/telegram_bot_app/handlers/user_command_handlers.py
@@ -1,8 +1,8 @@
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
-from src.telegram_bot_app.utils.texts import start_text
-from src.telegram_bot_app.keyboards.inline_keyboards import start_keyboard
+from src.telegram_bot_app.utils.texts import start_text, GENDER_TEXT
+from src.telegram_bot_app.keyboards.inline_keyboards import start_keyboard, gender_keyboard
 
 # Initiate router
 router = Router()
@@ -18,6 +18,15 @@ async def process_start_command(message: Message):
 
 
 # handling /help command
-@router.message(Command(commands='help'))
+@router.message(Command(commands="help"))
 async def process_help_command(message: Message):
     await message.answer(text="Помогите")
+
+
+# handling /gender command
+@router.message(Command(commands="gender"))
+async def process_gender_command(message: Message):
+    reply_text = GENDER_TEXT
+    keyboard = await gender_keyboard()
+    await message.answer(text=reply_text, reply_markup=keyboard)
+  
