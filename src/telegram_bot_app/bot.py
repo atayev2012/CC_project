cc67622549a115cc4e5e05008fe6041e6f3c5b36
Batyr Atayev <atayev2012@gmail.com>
2024-05-23 01:02:07 +0300

    Registered call router

--- src/telegram_bot_app/bot.py
+++ src/telegram_bot_app/bot.py
@@ -1,12 +1,14 @@
 from config import BOT_TOKEN
 from aiogram import Bot, Dispatcher
 from src.telegram_bot_app.handlers.user_command_handlers import router as commands_router
+from src.telegram_bot_app.handlers.user_call_handlers import router as call_router
 
 async def run_bot():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
     dp.include_router(commands_router)
+    dp.include_router(call_router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
